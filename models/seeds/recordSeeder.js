6f6430c01ad9a0918aa3a1de82adfcf2bc3d5379
Esther <moutain60237@gmail.com>
2020-08-09 16:18:12 +0800

    feat: modify recors date format

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -6,31 +6,31 @@ const records = [
   {
     name: '午餐',
     category: '餐飲食品',
-    date: '2020/08/01',
+    date: '2020-08-01',
     amount: '180',
   },
   {
     name: '加油',
     category: '交通出行',
-    date: '2020/08/01',
+    date: '2020-08-01',
     amount: '90',
   },
   {
     name: '電影票',
     category: '休閒娛樂',
-    date: '2020/08/03',
+    date: '2020-08-03',
     amount: '300',
   },
   {
     name: '買樂透',
     category: '其他',
-    date: '2020/08/03',
+    date: '2020-08-03',
     amount: '50',
   },
   {
     name: '停車費',
     category: '家居物業',
-    date: '2020/08/01',
+    date: '2020-08-01',
     amount: '3000',
   },
 ]
